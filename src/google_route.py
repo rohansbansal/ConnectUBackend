0574c5c4c8b1beb0d37fae6979626120dd8acf77
rohansbansal <rohansanjaybansal@gmail.com>
2020-08-27 15:07:15 -0400

    google auth

--- src/google_route.py
+++ src/google_route.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 google_auth = Blueprint("google_auth", __name__)
 
 
-@google_auth.route("/test", methods=["GET"])
+@google_auth.route("/login/google/authorized", methods=["GET"])
 def test():
     return "it works!"
